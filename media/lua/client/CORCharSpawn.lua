058bf67700ce35ae38c1c6e0a0f63eeb2fa0542a
Cory Fabian <Coryf65@gmail.com>
2021-12-18 13:48:10 -0600

    added the empty soda can. and removed nuka items not used

---
+++ media/lua/client/CORCharSpawn.lua
@@ -0,0 +1,21 @@
+local MOD_ID = "COR";
+local MOD_NAME = "Cory's Test Fizzy Soda";
+local MOD_VERSION = "1.0";
+local MOD_AUTHOR = "Cory Fabian";
+local MOD_DESCRIPTION = "Adds a Fizzy Soda to the game for a test mod.";
+local debugItems = TRUE;
+
+local function info()
+    print("Mod loaded. " .. MOD_NAME .. " by: " .. MOD_AUTHOR .. "(v ".. MOD_VERSION .. ")");
+end
+
+-- comment
+local function giveItems()
+    if debugItems then
+        local player = getSpecificPlayer(0);
+        player:getInventory():AddItem("COR.Soda");
+    end
+end
+
+Events.OnGameBoot.Add(info);
+Events.OnGameStart.Add(giveItems);
