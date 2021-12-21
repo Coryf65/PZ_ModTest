e50439d757b2d2fe0081834125adf08b267d3ebf
Cory Fabian <Coryf65@gmail.com>
2021-12-20 19:27:41 -0600

    working on new spawns

--- media/lua/client/CORCharSpawn.lua
+++ media/lua/client/CORCharSpawn.lua
@@ -5,6 +5,27 @@ local MOD_AUTHOR = "Cory Fabian";
 local MOD_DESCRIPTION = "Adds a Fizzy Soda to the game for a test mod.";
 local debugItems = TRUE;
 
+Soda = {}
+
+Soda.onLoad = function()
+    print("Chainsaw.onLoad")
+    local player = getPlayer()
+    Soda.initInvItems(player)
+end
+
+Soda.initInvItems = function(player)
+    local equippedChainsaw
+    local chainsaws
+    local petrolCans
+  
+    if player then
+      print("Soda.initInvItems: player is loaded")
+    else
+      print("Soda.initInvItems: player is null")
+      return
+    end
+end
+
 local function info()
     print("Mod loaded. " .. MOD_NAME .. " by: " .. MOD_AUTHOR .. "(v ".. MOD_VERSION .. ")");
 end
@@ -17,6 +38,4 @@ local function giveItems()
     end
 end
 
-Events.OnGameBoot.Add(info);
-Events.OnGameStart.Add(giveItems);
-Events.OnGameStart.Add(print("Test!"));
+Events.OnKeyPressed.Add(Chainsaw.addChainsaw)
